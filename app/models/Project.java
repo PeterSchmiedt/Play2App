fe29940be01987a6783c0e01557a387785d31443
PeterSchmiedt <schmiedt.p@gmail.com>
2014-01-12 00:58:22 +0100

    The model layer is ready => end of JavaGuide2

---
+++ app/models/Project.java
@@ -0,0 +1,44 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Peter on 12.1.2014.
+ */
+@Entity
+public class Project extends Model {
+
+    public static Model.Finder<Long, Project> find = new Model.Finder(Long.class, Project.class);
+    @Id
+    public Long id;
+    public String name;
+    public String folder;
+    @ManyToMany(cascade = CascadeType.REMOVE)
+    public List<User> members = new ArrayList<User>();
+
+    public Project(String name, String folder, User owner) {
+        this.name = name;
+        this.folder = folder;
+        this.members.add(owner);
+    }
+
+    public static Project create(String name, String folder, String owner) {
+        Project project = new Project(name, folder, User.find.ref(owner));
+        project.save();
+        project.saveManyToManyAssociations("members");
+        return project;
+    }
+
+    public static List<Project> findInvolving(String user) {
+        return find.where()
+                .eq("members.email", user)
+                .findList();
+    }
+}
---
+++ app/models/Task.java
@@ -0,0 +1,42 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Peter on 12.1.2014.
+ */
+@Entity
+public class Task extends Model {
+
+    public static Model.Finder<Long, Task> find = new Model.Finder(Long.class, Task.class);
+    @Id
+    public Long id;
+    public String title;
+    public boolean done = false;
+    public Date dueDate;
+    @ManyToOne
+    public User assignedTo;
+    public String folder;
+    @ManyToOne
+    public Project project;
+
+    public static List<Task> findTodoInvolving(String user) {
+        return find.fetch("project").where()
+                .eq("done", false)
+                .eq("project.members.email", user)
+                .findList();
+    }
+
+    public static Task create(Task task, Long project, String folder) {
+        task.project = Project.find.ref(project);
+        task.folder = folder;
+        task.save();
+        return task;
+    }
+}
